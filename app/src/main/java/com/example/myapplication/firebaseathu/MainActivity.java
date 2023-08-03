f4991281512f64f1393cfb812c50101930871c78
Amardeep Singh <adsingh26@gmail.com>
2023-08-03 10:58:29 +0530

    Add RegisterActivity

--- app/src/main/java/com/example/myapplication/firebaseathu/MainActivity.java
+++ app/src/main/java/com/example/myapplication/firebaseathu/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myapplication.firebaseathu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNewAccount = findViewById(R.id.createNewAccount);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
