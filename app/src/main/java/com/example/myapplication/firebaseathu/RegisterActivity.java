58e1e1f2ee6131a599e7314d3a1cb0d22f05bd78
Amardeep Singh <adsingh26@gmail.com>
2023-08-03 14:40:06 +0530

    Login & Register Using Firebase

--- app/src/main/java/com/example/myapplication/firebaseathu/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/firebaseathu/RegisterActivity.java
@@ -1,22 +1,54 @@
 package com.example.myapplication.firebaseathu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     TextView alreadyHaveAccount;
-    @Override
+    EditText inputEmail, inputPassword, inputConfirmPassword;
+    Button btnRegister;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+
+  // @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         alreadyHaveAccount = findViewById(R.id.alreadyHaveAccount);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassward);
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
         alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +57,59 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerforAuth();
+            }
+        });
+    }
+
+    private void PerforAuth() {
+            String email = inputEmail.getText().toString();
+            String password = inputPassword.getText().toString();
+            String confirmPassword = inputConfirmPassword.getText().toString();
+
+            if(!email.matches(emailPattern))
+            {
+                inputEmail.setError("Enter Correct Email");
+            }else if(password.isEmpty() || password.length()<6)
+            {
+
+                inputPassword.setError("Enter Proper Password");
+            }else if(!password.equals(confirmPassword))
+            {
+                inputConfirmPassword.setError("Password Not Match Both Fields");
+            }else
+            {
+                progressDialog.setMessage("Please wait While Registration.....");
+                progressDialog.setTitle("Registration.....");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegisterActivity.this, "Registation Successful", Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(RegisterActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
