b1a9c9b41b5805bf61fc4d67e26a38ba321ac8c2
Amardeep Singh <adsingh26@gmail.com>
2023-08-03 18:20:20 +0530

    Improve the Code

--- app/src/main/java/com/example/myapplication/firebaseathu/GoogleSignInActivity.java
+++ app/src/main/java/com/example/myapplication/firebaseathu/GoogleSignInActivity.java
@@ -3,8 +3,7 @@ package com.example.myapplication.firebaseathu;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ProgressBar;
+
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,8 +25,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class GoogleSignInActivity extends AppCompatActivity {
 
     private static final String TAG = "GoogleActivity";
-    private static final int RC_SIGN_IN = 9001;
-
+    private static final int RC_SIGN_IN = 101;
     private FirebaseAuth mAuth;
     FirebaseUser mUser;
     ProgressDialog progressDialog;
@@ -39,6 +37,7 @@ public class GoogleSignInActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Google Sign In...");
+        progressDialog.show();
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -46,26 +45,19 @@ public class GoogleSignInActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
-        setContentView(R.layout.activity_main); // Replace "activity_main" with your layout XML name
-    }
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
@@ -73,10 +65,8 @@ public class GoogleSignInActivity extends AppCompatActivity {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-
                 // Google Sign In failed, update UI appropriately
                 Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-                progressDialog.setMessage("Google Sign In...");
                 progressDialog.dismiss();
                 finish();
             }
@@ -96,19 +86,12 @@ public class GoogleSignInActivity extends AppCompatActivity {
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-
-                            Toast.makeText(GoogleSignInActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(GoogleSignInActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
                             finish();
-
                         }
                     }
                 });
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     private void updateUI(FirebaseUser user) {
